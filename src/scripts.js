cda84f9329b62f3e85f1549ade5dcb8fd197a16d
Daniel Curtin <dcurtin.work@gmail.com>
2023-02-28 18:21:07 -0700

    Add functionality to call api data

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,14 @@
 import './css/styles.css';
 import './images/bg-image.jpg';
+import getData from './api-calls';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let allCustomers, singleCustomer, allRooms, allBookings;
+
+Promise.all([getData('customers'), getData('customers/22'), getData('rooms'), getData('bookings')])
+.then(values => {
+    allCustomers = values[0].customers;
+    singleCustomer = values[1];
+    allRooms = values[2].rooms;
+    allBookings = values[3].bookings;
+    console.log(allCustomers, singleCustomer, allRooms, allBookings);
+});
