9f504201db556463f89bf0edd2537519fb677afd
Daniel Curtin <dcurtin.work@gmail.com>
2023-03-03 19:00:55 -0700

    Refactor customer and it's tests for functionality to be DRY and reusable

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -1,26 +1,40 @@
 class Hotel {
   constructor(allRooms, allBookings) {
     this.allRooms = allRooms;
-    this.allBookings = allBookings;
+    this.bookings = allBookings;
     this.filteredByDate = [];
     this.filteredByType = [];
     this.filteredBoth = [];
-  };
-
-  filterBookingsByType(filter) {
-    !this.filteredBookingsDate.length ? 
-    this.filteredBookingsType = this.allBookings.filter(booking => this.allRooms.find(room => booking.roomNumber === room.number) === filter) : 
-    this.filteredBoth = this.filteredBookingsDate.filter(booking => this.allRooms.find(room => booking.roomNumber === room.number) === filter);
-  };
-
-  selectDateBookings(date) {
-    !this.filteredBookingsType.length ?
-    this.filteredBookingsDate = this.allBookings.filter(booking => booking.date === date) :
-    this.filteredBoth = this.filteredBookingsType.filter(booking => booking.date === date);
+    this.selectedType;
+    this.selectedDate;
   };
 
   getRoomOfBooking(bookingRoomNum) {
     return this.allRooms.find(room => room.number === bookingRoomNum);
+  };
+
+  selectType(filter) {
+    this.selectedType = filter;
+    
+    !this.selectedDate ? this.filteredByType = this.bookings.filter(booking => this.allRooms.find(room => booking.roomNumber === room.number).type === filter) : this.filterBoth();
+  };
+
+  selectDate(date) {
+    this.selectedDate = date;
+
+    !this.selectedType ? this.filteredByDate = this.bookings.filter(booking => booking.date === date) : this.filterBoth();
+  };
+
+  filterBoth() {
+    this.filteredBoth = this.bookings.filter(booking => booking.date === this.selectedDate).filter(booking => this.allRooms.find(room => booking.roomNumber === room.number).type === this.selectedType);
+  };
+
+  resetDate() {
+    this.selectedDate = undefined;
+  };
+
+  resetType() {
+    this.selectedType = undefined;
   };
 };
 
--- test/Customer-test.js
+++ test/Customer-test.js
@@ -14,8 +14,8 @@ describe('Customer', () => {
     allBookings = bookings.map(booking => new Booking(booking, allRooms));
     hotel = new Hotel(allRooms, allBookings);
     
-    customer1 = new Customer(customers[0], hotel);
-    customer2 = new Customer(customers[1], hotel);
+    customer1 = new Customer(customers[0], allRooms, allBookings);
+    customer2 = new Customer(customers[1], allRooms, allBookings);
   });
 
   it('should be a function', () => {
@@ -36,16 +36,16 @@ describe('Customer', () => {
   });
 
   it('should be able to save the customers bookings', () => {
-    expect(customer1.allCustomerBookings).to.deep.equal([]);
-    expect(customer2.allCustomerBookings).to.deep.equal([]);
+    expect(customer1.hotel.bookings).to.deep.equal([]);
+    expect(customer2.hotel.bookings).to.deep.equal([]);
   });
 
   it('should only be able to save the customer\'s corresponding bookings', () => {
-    customer1.hotel.allBookings[0].userID = 1;
+    allBookings[0].userID = 1;
 
-    customer1.resetBookings();
+    customer1 = new Customer(customers[0], allRooms, allBookings);
 
-    expect(customer1.allCustomerBookings).to.deep.equal([allBookings[0]]);
+    expect(customer1.hotel.bookings).to.deep.equal([allBookings[0]]);
   });
 
   it('should be able to track the amount a customer has spent if they have no bookings', () => {
@@ -59,49 +59,50 @@ describe('Customer', () => {
   });
 
   it('should be able to track the amount a customer has spent if they have bookings', () => {
-    customer1.hotel.allBookings[0].userID = 1;
-    customer1.hotel.allBookings[0].roomNumber = 1;
+    allBookings[0].userID = 1;
+    allBookings[0].roomNumber = 1;
     
-    customer1.resetBookings();
+    customer1 = new Customer(customers[0], allRooms, allBookings);
     customer1.trackSpending();
 
     expect(customer1.spent).to.equal(358.4);
 
-    customer2.hotel.allBookings[0].userID = 2;
-    customer2.hotel.allBookings[0].roomNumber = 1;
-    customer2.hotel.allBookings[1].userID = 2;
-    customer2.hotel.allBookings[1].roomNumber = 2;
+    allBookings[0].userID = 2;
+    allBookings[0].roomNumber = 1;
+    allBookings[1].userID = 2;
+    allBookings[1].roomNumber = 2;
+
+    customer2 = new Customer(customers[1], allRooms, allBookings);
     
-    customer2.resetBookings();
     customer2.trackSpending();
 
     expect(customer2.spent).to.equal(835.78);
   });
 
   it('should be able to select a filter', () => {
-    customer1.selectType('suite');
+    customer1.hotel.selectType('suite');
 
-    expect(customer1.selectedType).to.equal('suite');
+    expect(customer1.hotel.selectedType).to.equal('suite');
 
-    customer2.selectType('single room');
+    customer2.hotel.selectType('single room');
 
-    expect(customer2.selectedType).to.equal('single room');
+    expect(customer2.hotel.selectedType).to.equal('single room');
   });
 
   it('should be able to select a date', () => {
-    customer1.selectDate('2022/02/11');
+    customer1.hotel.selectDate('2022/02/11');
 
-    expect(customer1.selectedDate).to.equal('2022/02/11');
+    expect(customer1.hotel.selectedDate).to.equal('2022/02/11');
 
-    customer2.selectDate('2023/10/05');
+    customer2.hotel.selectDate('2023/10/05');
 
-    expect(customer2.selectedDate).to.equal('2023/10/05');
+    expect(customer2.hotel.selectedDate).to.equal('2023/10/05');
   });
 
   it('should be able to save new bookings', () => {
-    customer1.selectDate('2023/06/12');
+    customer1.hotel.selectDate('2023/06/12');
     customer1.saveBooking(allRooms[0]);
 
-    expect(customer1.allCustomerBookings[0]).to.be.an.instanceOf(Booking);
+    expect(customer1.hotel.bookings[0]).to.be.an.instanceOf(Booking);
   });
 });
